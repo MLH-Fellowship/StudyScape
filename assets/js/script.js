6ac8fb497cbfe3c0a1cde62ed2593aaccd801c3b
Emily Amspoker <eamspoker21@kentdenver.org>
2020-10-11 13:01:45 -0600

    work on dragging

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
 dragula([ document.getElementById("dashboard_left"), document.getElementById("dashboard_right_up"), document.getElementById("dashboard_right_down"), document.getElementById("sidebar")], {
-  revertOnSpill: true
+  removeOnSpill: true
 });
       
