73407f5da88fdcbaea63e37fcfaa8ecfb1cda5f8
Shilpita Biswas <51776663+sh-biswas@users.noreply.github.com>
2020-10-13 07:56:35 -0700

    Add dashboard route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const app = express(); // This line should actually be "require('../app')" because it will import the app instanve created in app.js
-app.use(express.static(__dirname + 'public')); // The above line of code was implemented in order to make this one work and try to achieve the CSS files, but it still won't
+const app = require('../app'); // This line should actually be "require('../app')" because it will import the app instanve created in app.js
+//app.use(express.static(__dirname + 'public')); // The above line of code was implemented in order to make this one work and try to achieve the CSS files, but it still won't
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.sendFile('homepage.html', { root: 'views' }); // Homepage is the original landing page, assets is contained in /public
+});
+
+router.get('/dashboard', (req, res) => {
+    res.sendFile('dashboard.html', { root: 'views' }); // Homepage is the original landing page, assets is contained in /public
 });
 
 module.exports = router;
