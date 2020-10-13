08ff023b31a03d86f25fabda8895cef2c45bf7d3
Shilpita Biswas <51776663+sh-biswas@users.noreply.github.com>
2020-10-13 01:41:09 -0700

    Debug rendering for Express

---
+++ views/assets/js/script.js
@@ -0,0 +1,4 @@
+dragula([ document.getElementById("dashboard_left"), document.getElementById("dashboard_right_up"), document.getElementById("dashboard_right_down"), document.getElementById("sidebar")], {
+  removeOnSpill: true
+});
+      
