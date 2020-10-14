97abf69825b89c0077f43d5227aaf99a33a9122e
Shilpita Biswas <51776663+sh-biswas@users.noreply.github.com>
2020-10-13 21:01:13 -0700

    Demo

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const routes = require('./routes/index');
+const path = require('path');
 
 const app = express();
 app.use('/', routes);
-app.use(express.static('public'));
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 module.exports = app;
--- public/assets1/js/script.js
+++ public/assets1/js/script.js
@@ -1,4 +1,193 @@
+//DragulaJS for front-end dragging
 dragula([ document.getElementById("dashboard_left"), document.getElementById("dashboard_right_up"), document.getElementById("dashboard_right_down"), document.getElementById("sidebar")], {
-  removeOnSpill: true
+  removeOnSpill: true,
+  copy: function (el, source) {
+    return source === document.getElementById("sidebar")
+  },
+  accepts: function (el, target) {
+    return target !== document.getElementById("sidebar")
+  }
 });
-      
+
+//code for to-do list
+loadEvents();
+function loadEvents(){
+  document.querySelector('form').addEventListener('submit',submit);
+  document.getElementById('clear').addEventListener('click',clearList);
+  document.querySelector('ul').addEventListener('click',deleteOrTick);
+
+}
+
+function submit(e){
+  e.preventDefault();
+  let taskList;
+  let input = document.querySelector('input');
+  if(input.value != '')
+    addTask(input.value);
+  input.value = '';
+}
+
+
+function addTask(task){
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  li.innerHTML = `<span class="delete">×</span><label>${task}</label><input type="checkbox">`;
+  ul.appendChild(li);
+  document.querySelector('.tasksBoard').style.display = 'block';
+}
+
+function clearList(e){
+  let ul = document.querySelector('ul').innerHTML = '';
+}
+
+function deleteOrTick(e){
+  if(e.target.className == 'delete')
+    deleteTask(e);
+  else {
+    tickTask(e);
+  }
+}
+
+function deleteTask(e){
+  let remove = e.target.parentNode;
+  let parentNode = remove.parentNode;
+  parentNode.removeChild(remove);
+}
+
+function tickTask(e){
+  const task = e.target.nextSibling;
+  if(e.target.checked){
+    task.style.textDecoration = "line-through";
+    task.style.color = "#ff0000";
+  }else {
+    task.style.textDecoration = "none";
+    task.style.color = "#2f4f4f";
+  }
+}
+
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '298355234726-b0p7rd8qv7a775cqjn67t87nr9vt1k24.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyCS8hSlbnflnSGa1qehCaGMZgzWEwsTsW4';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+  gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scope: SCOPES
+  }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
+  }, function(error) {
+    appendPre(JSON.stringify(error, null, 2));
+  });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'block';
+    listUpcomingEvents();
+  } else {
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
+  }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+  deleteContent();
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+  var pre = document.getElementById('content');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+
+/**
+ * Deletes the content pre element. Used to prevent redundancy
+ * when the displaying results of the API call if the user signs out and in or
+ * refreshes the calendar.
+ */
+function deleteContent(){
+  document.getElementById('content').innerHTML = "";
+}
+
+/**
+ * Print the summary and start datetime/date of the next ten events in
+ * the authorized user's calendar. If no events are found an
+ * appropriate message is printed.
+ */
+function listUpcomingEvents() {
+  gapi.client.calendar.events.list({
+    'calendarId': 'primary',
+    'timeMin': (new Date()).toISOString(),
+    'showDeleted': false,
+    'singleEvents': true,
+    'maxResults': 10,
+    'orderBy': 'startTime'
+  }).then(function(response) {
+    var events = response.result.items;
+    appendPre('Upcoming events:');
+
+    if (events.length > 0) {
+      for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        var when = event.start.dateTime;
+        if (!when) {
+          when = event.start.date;
+        }
+        appendPre(event.summary + ' (' + when + ')')
+      }
+    } else {
+      appendPre('No upcoming events found.');
+    }
+  });
+}
